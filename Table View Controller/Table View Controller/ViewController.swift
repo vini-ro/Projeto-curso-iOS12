4072fab08c85a89588c4c90b4b97fcaa14d4e687
Vinicius <vroliveira.eng@gmail.com>
2024-08-20 13:51:01 -0300

    Primeiro Commit Table View

---
+++ Table View Controller/Table View Controller/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Table View Controller
+//
+//  Created by Vinicius Oliveira on 20/08/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var dados: [String] = ["Lasanha", "Pizza", "Torta"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return dados.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: <#T##IndexPath#>)
+        celula.textLabel?.text = dados [indexPath.row]
+    }
+    
+}
+
